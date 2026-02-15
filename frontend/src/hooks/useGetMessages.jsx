ef9c5ce8bed00e6a03e624b0682decdbe853e699
Dev Ashish <dshish719@gmail.com>
2026-02-15 21:06:09 +0530

    update

--- frontend/src/hooks/useGetMessages.jsx
+++ frontend/src/hooks/useGetMessages.jsx
@@ -37,34 +37,3 @@ export default useGetMessages;
 
 
 
-// import { useEffect } from "react";
-// import axios from "axios";
-// import { useDispatch, useSelector } from "react-redux";
-// import { setMessages } from "../redux/messageSlice";
-
-// const useGetMessages = () => {
-//   const dispatch = useDispatch();
-//   const { selectedUser } = useSelector((state) => state.user);
-
-//   useEffect(() => {
-//     if (!selectedUser) return;
-
-//     const fetchMessages = async () => {
-//       try {
-//         const res = await axios.get(
-//           `http://localhost:5000/api/message/${selectedUser._id}`,
-//           { withCredentials: true }
-//         );
-
-//         // 🔥 YAHI LINE MISS HOTI HAI AKSAR
-//         dispatch(setMessages(res.data.messages || []));
-//       } catch (error) {
-//         console.log("Error fetching messages", error);
-//       }
-//     };
-
-//     fetchMessages();
-//   }, [selectedUser, dispatch]);
-// };
-
-// export default useGetMessages;
